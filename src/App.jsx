bb53396d698dc9e5b79d92fd5a708f9d8e00ccfb
Anjali N V <anjaliprathees.nv@gmail.com>
2024-01-05 21:05:01 +0530

    fix: router dom deployment bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import "./App.css";
 import HomeLayout from "./layouts/HomeLayout";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Bookmarks from "./components/Bookmarks/Bookmarks";
 
 function App() {
   return (
-    <Router>
-      <HomeLayout>
-        <Routes>
-          <Route path="/" exact Component={Home} />
-          <Route path="/bookmarks" Component={Bookmarks} />
-        </Routes>
-      </HomeLayout>
-    </Router>
+    <HomeLayout>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/bookmarks" element={<Bookmarks />} />
+      </Routes>
+    </HomeLayout>
   );
 }
 
